e9dd878876271e23035e94d3caa1a6ef44ad596a
Ingrid Munera <ingridmunera24@gmail.com>
2026-04-16 19:33:22 -0500

    Set starting point and parrallel execution config

--- src/test/java/org/testcompany/eCommerce_tc_2.java
+++ src/test/java/org/testcompany/eCommerce_tc_2.java
@@ -1,15 +1,23 @@
 package org.testcompany;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class eCommerce_tc_2 extends BaseTest {
+    @BeforeMethod
+    public void preSetup(){
+        //set screen to home page before running any test
+        Activity activity = new Activity("com.androidsample.generalstore","com.androidsample.generalstore.SplashActivity");
+        ((JavascriptExecutor) driver).executeScript("mobile: startActivity", ImmutableMap.of(
+                "intent","com.androidsample.generalstore/com.androidsample.generalstore.SplashActivity"));
+    }
+
     @Test
     public void FillForm_ErrorValidation() throws InterruptedException {
         //driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Ingrid Munera");
@@ -24,8 +32,7 @@ public class eCommerce_tc_2 extends BaseTest {
         //Click on Let's shop button without entering the name
         driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
         String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
-        Assert.assertEquals(toastMessage, "Please enter your name ");
-
+        Assert.assertEquals(toastMessage, "Please enter your name");
     }
 
     @Test
